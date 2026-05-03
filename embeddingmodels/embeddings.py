b7adbf2b25d3244507fc93c01c0f10b052433898
Abhay Vaishnav <vaishnavabhay536@gmail.com>
2026-05-04 02:04:13 +0530

    this is an example of embeddings working

---
+++ embeddingmodels/embeddings.py
@@ -0,0 +1,18 @@
+from langchain_mistralai import MistralAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = MistralAIEmbeddings(
+    model="mistral-embed"
+)
+
+texts = [
+    "Hello this is Abhay Vaishnav",
+    "Hello your name is YouTube",
+    "And you all are very beautiful"
+]
+
+vector = embeddings.embed_documents(texts)
+
+print(vector)
